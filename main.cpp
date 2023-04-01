e822c22d547b1aa31c92c42635554c2dfbc66c5f
Sasha Kozyritskaya <55165756+aleksandrajk@users.noreply.github.com>
2023-04-01 12:51:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 
 int main(int argc, char** argv) {
-  Image test("/Users/alexandra/Downloads/2122_PJC_pl_s24228/images/lake.jpg");
+  // Image test("/Users/alexandra/Downloads/2122_PJC_pl_s24228/images/lake.jpg");
   test.encodeMessage("This is my secret messages to hide");
   test.write("Secret_lake.jpg");
 
   char buffer[256] = {0};
   size_t len = 0;
-  //below is the function' call that I could use to decode image
+  
   // test.decodeMessage(buffer, &len);
 
     return 0;
